df99eb27dc339bb79fe0a2c3ac6f60b102e8893d
HaleyOtaku <haleyotaku@gmail.com>
2024-05-27 13:54:10 -0500

    Add project files.

---
+++ C# ToDoList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
